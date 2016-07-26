4e6090a0543641cc017c30f6827a8b6b129c3c91
rittersm <rittersm@gmail.com>
2016-07-25 21:32:40 -0400

    corrected intentional errors in email

--- app/mailers/user_updates_mailer.rb
+++ app/mailers/user_updates_mailer.rb
@@ -6,7 +6,7 @@ class UserUpdatesMailer < ApplicationMailer
   end
 
   def following_email(user, current_user)
-    @userish = users
+    @user = user
     @user2 = current_user
     mail( :to => @user.email, :subject => 'Someone has followed you on Chirp')
   end
